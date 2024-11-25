7af80d0154bf38ef84068192dc6e76a4185bb46a
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-25 14:26:00 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -7,44 +7,45 @@ private:
     double width;
 
 public:
-    // Constructor
+    // Constructor to initialize attributes
     Rectangle() : length(0), width(0) {}
 
-    // Member function to set dimensions
+    // Function to set dimensions
     void setDimensions(double len, double wid) {
         if (len > 0 && wid > 0) {
             length = len;
             width = wid;
         } else {
-            cout << "Length and width must be positive values." << endl;
+            cout << "Length and width must be positive values.\n";
         }
     }
 
-    // Member function to calculate area
+    // Function to calculate the area of the rectangle
     double calculateArea() const {
         return length * width;
     }
 
-    // Member function to display dimensions and area
+    // Function to display dimensions and area
     void display() const {
-        cout << "Rectangle dimensions: " << endl;
-        cout << "Length: " << length << " Width: " << width << endl;
-        cout << "Area: " << calculateArea() << endl;
+        cout << "Rectangle Dimensions:" << endl;
+        cout << "Length: " << length << " units" << endl;
+        cout << "Width: " << width << " units" << endl;
+        cout << "Area: " << calculateArea() << " square units" << endl;
     }
 };
 
 int main() {
     Rectangle rect;
-
     double len, wid;
+
+    // Input dimensions from the user
     cout << "Enter the length of the rectangle: ";
     cin >> len;
     cout << "Enter the width of the rectangle: ";
     cin >> wid;
 
+    // Set dimensions and display the details
     rect.setDimensions(len, wid);
-
-    cout << endl;
     rect.display();
 
     return 0;
